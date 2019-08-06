62bfe21a350b56424f0c205e7b287efcf890732e
tatjanatanaskovich <tatjanatanaskovich@gmail.com>
2019-08-06 14:25:32 +0200

    Added card and card-list components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Monsters App
-        </p>
-      </header>
-    </div>
-  );
+class  App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+      <CardList monsters = {this.state.monsters} />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
